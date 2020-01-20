883fb811ae37806fb72e5f1c0093d3b516d98a4e
jomut9 <32397316+jomut9@users.noreply.github.com>
2020-01-21 02:23:20 +0800

    Create lihkg-imgur-viewer.user.js

---
+++ lihkg-imgur-viewer.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         LIHKG imgur viewer
+// @namespace    https://github.com/abcde
+// @version      0.1
+// @description  View media hosted on imgur
+// @author       jomut9
+// @include      *://lihkg.com/thread/*
+// @grant        GM_xmlhttpRequest
+// @run-at       document-idle
+// ==/UserScript==
+
+(function (){
+    const observer = new MutationObserver((mutations) => {
+        for (const mutation of mutations) {
+            if (mutation.type === 'childList') {
+                for (const node of mutation.addedNodes) {
+                    if (node.nodeType === document.ELEMENT_NODE) {
+                        var els = node.querySelectorAll("a[href^='https://imgur.com/']");
+                        if(els.length != 0){
+                            for (const el of els){
+                                var split = el.getAttribute('href').split('/');
+                                var bq = document.createElement("blockquote");
+                                var dataid = split[3] + "/" + split[4];
+                                bq.className = "imgur-embed-pub";
+                                bq.lang = 'en';
+                                bq.dataset.id = dataid;
+                                var a = document.createElement("a");
+                                a.href = "//" + split[2] + "/" + split[3] + "/" + split[4];
+                                bq.appendChild(a);
+
+                                var script = document.createElement("script");
+                                script.async = "";
+                                script.src = "//s.imgur.com/min/embed.js";
+                                script.charset = "utf-8";
+                                el.insertAdjacentElement("afterend", bq);
+                                el.insertAdjacentElement("afterend", script);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
